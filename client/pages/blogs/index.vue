00ee436ccb15b03e14bf725e83b9178c563f3d2d
Nemanja Danev <nemanja.danev.93@gmail.com>
2021-10-28 11:07:13 -0400

    added blogs page

--- client/pages/blogs/index.vue
+++ client/pages/blogs/index.vue
@@ -1,9 +1,64 @@
 <template>
-  <div class="container">
-    <h1>Blogs page</h1>
+  <div class="blogs">
+    <div class="hero">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1 class="hero-title">
+              Blogs
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="section">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h2 class="section-title">
+            Read our <span>blogs</span>
+          </h2>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div v-for="(article, index) in articles" :key="index" class="col-md-8 mb-3">
+            <nuxt-link :to="{ name: 'blog-slug', params:{slug: article.slug}}" class="d-block blog-card mb-3 mb-md-0">
+              <img :src="require(`@/assets/images/${article.image}`)" class="img-fluid" alt>
+              <div class="details">
+                <h3>
+                  {{ article.title }}
+                </h3>
+                <p>
+                  {{ article.desc }}
+                </p>
+              </div>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData ({ $content, params }) {
+    const articles = await $content('blog', params.slug).only(['title', 'desc', 'image', 'slug']).sortBy('createdAt', 'asc').fetch()
+    return { articles }
+  }
+}
 </script>
+
+<style>
+.blogs .section-title {
+  color: #212529;
+}
+
+.blogs .section-title span {
+  color: #f33c7a;
+}
+
+.blogs .section-title span:before {
+  display: none;
+}
+</style>
